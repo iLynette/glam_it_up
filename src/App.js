1999296833268f90e4bbd940ccf9f59bebb5a6fc
iLynette <lynetteacholah@icloud.com>
2022-04-18 08:38:07 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import HomePage from "./Components/HomePage";
+import DetailsPage from "./Components/DetailsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="nav">
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/detailspage" element={<DetailsPage />} />
+    </Routes>
+  </div>
+);
 
 export default App;
