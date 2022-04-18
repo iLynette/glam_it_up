10ff97396c09308d23076edeba1b3932a07704f8
iLynette <lynetteacholah@icloud.com>
2022-04-18 08:15:46 +0300

    Add navigation

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => (
+    <nav className="navBar">
+      <h3>Glam It Up</h3>
+      <div className="navItems">
+        <ul className="navMenu">
+          <li><NavLink className="homepage" to="/">HomePage</NavLink></li>
+          <li><NavLink className="detailspage" to="/detailspage">DetailsPage </NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+  
+  export default Navbar;
