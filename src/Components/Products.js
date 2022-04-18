db14ca692d7fa8eae78207ab22a82ebd54b2d22a
iLynette <lynetteacholah@icloud.com>
2022-04-18 20:24:43 +0300

    Set parameters for products

---
+++ src/Components/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Proptypes from 'prop-type';
+
+const Products = (props) => {
+    const { product } = props;
+    const {
+        id, brand, name, image, price
+    } = product
+}
+
+
+export default Products
