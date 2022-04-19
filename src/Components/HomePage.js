711e1231f71ff1d46552747429f3e4dd9af09282
iLynette <lynetteacholah@icloud.com>
2022-04-19 13:30:03 +0300

    Display a list of products from the API

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -11,7 +11,21 @@ const HomePage = () => {
     dispatch(fetchProductFromApi());
   }, []);
   return (
-
+    <>
+    <div className="products">
+     {data.map((product) => (
+       <div key={product.id}>
+         <p key={product.id}>{product.id}</p>
+         <p key={product.id}>{product.name}</p>
+         <p key={product.id}>{product.price}</p>
+         <p key={product.id}>{product.description}</p>
+         <p key={product.id}>
+           <img src={product.image_link} alt={product.title} />
+         </p>
+       </div>
+  ))}
+     </div>
+    </>
   );
 }
 
