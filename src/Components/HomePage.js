c1a33b98c59200c16779cf3695dbb84fd82367b3
iLynette <lynetteacholah@icloud.com>
2022-04-19 15:11:51 +0300

    Add styles to homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,7 +5,7 @@ import { fetchProductFromApi } from '../redux/Products/products';
 const HomePage = () => {
   const data = useSelector((state) => state.productReducer);
 
-  console.log(data, 'print this');
+  // console.log(data, 'print this');
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchProductFromApi());
@@ -15,13 +15,13 @@ const HomePage = () => {
       <div className="products">
         {data.map((product) => (
           <div key={product.id}>
-            <p key={product.id}>{product.id}</p>
-            <p key={product.id}>{product.name}</p>
-            <p key={product.id}>{product.price}</p>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+            <p>{product.id}</p>
             {/* <p key={product.id}>{product.description}</p> */}
-            <p key={product.id}>
+            {/* <p key={product.id}>
               <img src={product.image_link} alt={product.title} />
-            </p>
+            </p> */}
           </div>
         ))}
       </div>
