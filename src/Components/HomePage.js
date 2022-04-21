0924d08645d40ec2d0385ef89cc8fa4c17a1f998
iLynette <lynetteacholah@icloud.com>
2022-04-21 13:55:13 +0300

    Import useState and filter through categories

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,19 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProductFromApi } from '../redux/Products/products';
+import Categories from './Categories';
+import Btn from './Arrow';
 
 const HomePage = () => {
   const data = useSelector((state) => state.productReducer);
+  const [value, setValue] = useState('');
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchProductFromApi());
   }, []);
+
+  const handleClick = (e) => {
+    setValue(e.target.value);
+  };
+  const filterProducts = data.filter((product) =>
+    product.product_type.toLowerCase().includes(value.toLowerCase()));
   return (
     <>
+    <Categories data={data} handleClick={handleClick} />
       <div className="products">
-        {data.map((product) => (
+        {value === 'Select a product type'
+         ? data.map((product) => (
           <div key={product.id}>
-            <p>{product.name}</p>
+            <p className="head">
+              {product.name}
+              <Btn id={product.id} />
+            </p>
             <p>{product.product_type}</p>
             <p>{product.price}</p>
             {/* <p key={product.id}>{product.description}</p> */}
@@ -21,7 +35,8 @@ const HomePage = () => {
               <img src={product.image_link} alt={product.title} />
             </p> */}
           </div>
-        ))}
+        ))
+        
       </div>
     </>
   );
