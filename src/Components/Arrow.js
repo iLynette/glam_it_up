7b48028ce184ef3c9ea6d04ea0a7abac674d85e9
iLynette <lynetteacholah@icloud.com>
2022-04-21 14:18:18 +0300

    Add arrow and include a handleclick event to it

--- src/Components/Arrow.js
+++ src/Components/Arrow.js
@@ -3,3 +3,14 @@ import { HiArrowCircleRight } from "react-icons/hi";
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+const Btn = (props) => {
+    const { id } = props;
+    const { handleClick } = props;
+    return (
+        <NavLink className="btn" onClick={handleClick} to={`/details/${id}`}>
+            <HiArrowCircleRight />
+        </NavLink>
+    )
+}
+
+export default Btn
