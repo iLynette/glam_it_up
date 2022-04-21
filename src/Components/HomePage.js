4604c7310c72f26eae46d46d71876918cab09906
iLynette <lynetteacholah@icloud.com>
2022-04-21 18:25:52 +0300

    Add a class name to a div and fix linter errors

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -15,42 +15,44 @@ const HomePage = () => {
   const handleClick = (e) => {
     setValue(e.target.value);
   };
-  const filterProducts = data.filter((product) =>
-    product.product_type.toLowerCase().includes(value.toLowerCase()));
+  const filterProducts = data.filter((product) => product.product_type.toLowerCase().includes(value.toLowerCase()));
   return (
     <>
-    <Categories data={data} handleClick={handleClick} />
+      <Categories data={data} handleClick={handleClick} />
       <div className="products">
         {value === 'Select a product type'
-         ? data.map((product) => (
-          <div key={product.id}>
-            <p className="head">
-              {product.name}
-              <Btn id={product.id} />
-            </p>
-            <p>{product.product_type}</p>
-            <p>{product.price}</p>
-            {/* <p key={product.id}>{product.description}</p> */}
-            {/* <p key={product.id}>
+          ? data.map((product) => (
+            <div key={product.id}>
+              <p className="head">
+                {product.name}
+              </p>
+              <span>
+                <Btn id={product.id} />
+              </span>
+              <p>{product.product_type}</p>
+              <p>{product.price}</p>
+              {/* <p key={product.id}>{product.description}</p> */}
+              {/* <p key={product.id}>
               <img src={product.image_link} alt={product.title} />
             </p> */}
-          </div>
-        ))
-        : filterProducts.map((product) => (
-          <div key={product.id}>
-            <p className="title" id={product.id}>
-              {product.name}
-              <Btn id={product.id} />
-            </p>
-            <p className="product-type" id={product.id}>
-              {product.product_type}
-            </p>
-            <p className="product-price" id={product.id}>
-              {product.price}
-            </p>
-          </div>
-        )
-        )}
+            </div>
+          ))
+          : filterProducts.map((product) => (
+            <div key={product.id}>
+              <div className="min">
+                <p className="title" id={product.id}>
+                  {product.name}
+                </p>
+                <Btn id={product.id} />
+              </div>
+              <p className="product-type" id={product.id}>
+                {product.product_type}
+              </p>
+              <p className="product-price" id={product.id}>
+                {product.price}
+              </p>
+            </div>
+          ))}
       </div>
     </>
   );
