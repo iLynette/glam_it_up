cd90a71a6edc112b0927fb96cab396f30320a897
iLynette <lynetteacholah@icloud.com>
2022-04-21 13:46:38 +0300

    Use a drop down menu for the product types

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -6,6 +6,18 @@ const Categories = (props) => {
     const data = props;
     const newItem = [... new Set(data.data.map((makeup) => makeup.product_type))];
     const { handleClick } = props;
+
+    return (
+        <select className="items" onChange={(e) => setproduct_type(e.target.value)} onClick={handleClick} required>
+            <option value="select a product type">select a product type</option>
+            <option value={product_type}></option>
+            {newItem.map((product_type) => (
+                <option key={product_type} value={product_type}>
+                    {product_type}
+                </option>
+            ))}
+        </select>
+    )
 }
 
 export default Categories
