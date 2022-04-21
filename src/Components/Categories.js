26736ee8c728323d23aeff2d94ee271947da946c
iLynette <lynetteacholah@icloud.com>
2022-04-21 18:25:20 +0300

    Fix Linter errors

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Categories = (props) => {
-    const [product_type, setproduct_type] = useState('');
-    const data = props;
-    const newItem = [... new Set(data.data.map((makeup) => makeup.product_type))];
-    const { handleClick } = props;
+  const [productType, setProductType] = useState('');
+  const data = props;
+  const newItem = [...new Set(data.data.map((makeup) => makeup.productType))];
+  const { handleClick } = props;
 
-    return (
-        <select className="items" onChange={(e) => setproduct_type(e.target.value)} onClick={handleClick} required>
-            <option value="select a product type">select a product type</option>
-            <option value={product_type}></option>
-            {newItem.map((product_type) => (
-                <option key={product_type} value={product_type}>
-                    {product_type}
-                </option>
-            ))}
-        </select>
-    )
-}
+  return (
+    <select className="items" onChange={(e) => setProductType(e.target.value)} onClick={handleClick} required>
+      <option value="select a product type">select a product type</option>
+      <option value={productType} />
+      {newItem.map((productType) => (
+        <option key={productType} value={productType}>
+          {productType}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 Categories.propTypes = {
-    handleClick: PropTypes.func.isRequired,
-}
+  handleClick: PropTypes.func.isRequired,
+};
 
-export default Categories
+export default Categories;
