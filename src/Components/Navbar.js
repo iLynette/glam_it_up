0843d94500760ccc364103dce4b703ef9dfa1b83
iLynette <lynetteacholah@icloud.com>
2022-04-21 18:26:23 +0300

    Change the navigation bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiMicrophone } from 'react-icons/hi';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
 
 const Navbar = () => (
-  <nav className="navBar">
-    <h3>Glam It Up</h3>
-    <div className="navItems">
-      <ul className="navMenu">
-        <li><NavLink className="homepage" to="/">HomePage</NavLink></li>
-        <li><NavLink className="detailspage" to="/detailspage">DetailsPage </NavLink></li>
+  <nav className="navigationBar">
+    <div className="bar">
+      <ul className="navMenus">
+        <li>
+          <NavLink className="home" to="/">
+            <MdKeyboardArrowLeft />
+          </NavLink>
+        </li>
       </ul>
+      {/* <MdOutlineSentimentVerySatisfied /> */}
+      <h3>
+        Glam It Up
+      </h3>
+      <HiMicrophone />
     </div>
+    {/* <div className="nav">
+      <ul className="navMenus">
+        <li>
+          <NavLink className="home" to="/">
+            <MdKeyboardArrowLeft />
+          </NavLink>
+        </li>
+      </ul>
+    </div> */}
   </nav>
 );
 
