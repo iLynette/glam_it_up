0b0bf8d8c7667e20d26dcdb6669202938d921e9a
iLynette <lynetteacholah@icloud.com>
2022-04-21 13:45:29 +0300

    Initialize category constants

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,2 +1,11 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
+
+const Categories = (props) => {
+    const [product_type, setproduct_type] = useState('');
+    const data = props;
+    const newItem = [... new Set(data.data.map((makeup) => makeup.product_type))];
+    const { handleClick } = props;
+}
+
+export default Categories
