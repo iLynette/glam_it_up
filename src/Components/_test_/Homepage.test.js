d537b05fedaa79a4084971abf061c74e0901c856
iLynette <lynetteacholah@icloud.com>
2022-04-22 12:19:12 +0300

    Test the home page

--- src/Components/_test_/Homepage.test.js
+++ src/Components/_test_/Homepage.test.js
@@ -1,1 +1,16 @@
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import HomePage from '../HomePage';
+import store from '../../redux/configStore';
+const HomePageProvider = () => (
+    <Provider store={store}>
+        <HomePage />
+    </Provider>
+)
+
+describe('The categories render properly', () => {
+    it('renders correctly', () => {
+        const component = renderer.create(<HomePageProvider />).toJSON()
+        expect(component).toMatchSnapshot();
+    })
+})
