53ed5b9beba7b32faeb88292de461b61b3440dd4
iLynette <lynetteacholah@icloud.com>
2022-04-22 14:12:50 +0300

    Fix linters

--- src/Components/_test_/UI.test.js
+++ src/Components/_test_/UI.test.js
@@ -1,29 +1,20 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import HomePage from '../HomePage';
 import store from '../../redux/configStore';
-import DetailsPage from '../DetailsPage';
 
 const HomePageProvider = () => (
-  <Provider store={store}>
-    <Router>
-      <HomePage />
-    </Router>
-  </Provider>
-);
+    <Provider store={store}>
+        <Router>
+            <HomePage />
+        </Router>
+    </Provider>
+)
 
-const DetailsPageProvider = () => (
-  <Provider store={store}>
-    <Router>
-      <DetailsPage />
-    </Router>
-  </Provider>
-);
-
-describe('renders details properly', () => {
-  it('UI renders correctly', () => {
-    const detailsPage = renderer.create(<DetailsPageProvider />).toTree();
-    expect(detailsPage).toMatchSnapshot();
-  });
-});
+describe('The homepage renders its UI properly', () => {
+    it('UI renders correctly', () => {
+        const homePage = renderer.create(<HomePageProvider />).toTree()
+        expect(homePage).toMatchSnapshot();
+    })
+})
