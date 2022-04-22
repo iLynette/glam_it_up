22d3b8bf84e7be96f46d3877d91e4cba5f9009dc
iLynette <lynetteacholah@icloud.com>
2022-04-22 13:09:47 +0300

    Add test for details page

--- src/Components/_test_/UI.test.js
+++ src/Components/_test_/UI.test.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import HomePage from '../HomePage';
 import store from '../../redux/configStore';
+import DetailsPage from "../DetailsPage";
 
 const HomePageProvider = () => (
     <Provider store={store}>
@@ -12,9 +13,17 @@ const HomePageProvider = () => (
     </Provider>
 )
 
-describe('The homepage renders its UI properly', () => {
+const DetailsPageProvider = () => (
+    <Provider store={store}>
+        <Router>
+            <DetailsPage />
+        </Router>
+    </Provider>
+)
+
+describe('renders details properly', () => {
     it('UI renders correctly', () => {
-        const homePage = renderer.create(<HomePageProvider />).toTree()
-        expect(homePage).toMatchSnapshot();
+        const detailsPage = renderer.create(<DetailsPageProvider />).toTree()
+        expect(detailsPage).toMatchSnapshot();
     })
 })
