49b500748ff3d70145fc2e4538c4dd4ef3b8552d
iLynette <lynetteacholah@icloud.com>
2022-04-22 14:05:02 +0300

    Add test for reducer

---
+++ src/Components/_test_/ProductReducer.test.js
@@ -0,0 +1,9 @@
+import productReducer from "../../redux/Products/products";
+
+const initialState = [];
+
+describe('Tests for redux in project', () => {
+    it('Tests if the state is initially empty', () => {
+        expect(productReducer(initialState, [])).toEqual([])
+    })
+})
