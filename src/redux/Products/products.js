08e2a3c5b9178377400b5cc8a3bc07a164b3889a
iLynette <lynetteacholah@icloud.com>
2022-04-22 19:28:04 +0300

    Make changes to reducer

--- src/redux/Products/products.js
+++ src/redux/Products/products.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
 const FETCH_PRODUCTS = 'glam_it_up/redux/Product/FETCH_PRODUCTS';
+const FILTER_PRODUCTS = 'glam_it_up/redux/FILTER_PRODUCTS';
 
-const defaultState = [];
+const defaultState = {
+  products: [],
+  filter: [],
+  filter_value: 'Select a product type',
+};
 
 export const fetchProducts = (payload) => ({
   type: FETCH_PRODUCTS,
   payload,
+});
+
+export const filterReduxProducts = (filter) => ({
+  type: FILTER_PRODUCTS,
+  payload: filter,
 });
 
 export const fetchProductFromApi = () => async (dispatch) => {
@@ -19,9 +29,23 @@ export const fetchProductFromApi = () => async (dispatch) => {
 const productReducer = (state = defaultState, action) => {
   switch (action.type) {
     case FETCH_PRODUCTS:
-      return [
-        ...action.payload.data,
-      ];
+      return {
+        products: [
+          ...action.payload.data,
+        ],
+        filter: [
+          ...action.payload.data,
+        ],
+      };
+    case FILTER_PRODUCTS:
+      return {
+        ...state,
+        filter: [
+          ...state.products
+            .filter((p) => p.product_type.toLowerCase().includes(action.payload.toLowerCase())),
+        ],
+        filter_value: action.payload,
+      };
     default:
       return state;
   }
